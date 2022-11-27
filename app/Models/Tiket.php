dee093f29bad2d3dced2bffe0776d537451755af
aliya <92076520+aliya25@users.noreply.github.com>
2022-11-27 13:47:01 +0700

    ehtic

---
+++ app/Models/Tiket.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tiket extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'nama','no_pesanan','section','harga','date_of_issue'
+    ]; 
+}
